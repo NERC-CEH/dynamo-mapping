3b823ceceb1e07743e9bcffd41ffbefdfee74081
Christopher Johnson <cjohn@ceh.ac.uk>
2014-05-02 11:28:14 +0100

    Adding an implementation of Climate which can be updated at will

---
+++ src/main/java/uk/ac/ceh/dynamo/bread/UpdatableClimate.java
@@ -0,0 +1,15 @@
+package uk.ac.ceh.dynamo.bread;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * A simple lombok wrapper around a climate. It will create with a default
+ * climate of 1 but can be manipulated later
+ * @author Christopher Johnson
+ */
+@Data
+@AllArgsConstructor
+public class UpdatableClimate implements Climate {
+    private double currentClimate = 1d;
+}
